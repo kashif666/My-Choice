e768057948e13a50a4454086654ced78b72c4a27
Syed Kashif Ali Shah <114756954+kashif666@users.noreply.github.com>
2022-10-18 21:59:08 -0700

    Added more code

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,15 @@
-import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
+import "./OnlyCss.css";
 function HomePage() {
-
+const mystyle =
+{
+  backgroundColor: "lightBlue",
+  
+};
 
     return ( 
-<div>
-<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+<div style={mystyle}>
+<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist" style={{position: 'fixed',top:'0vw',width:'99vw'}} >
   <li class="nav-item" role="presentation">
     <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
   </li>
@@ -16,14 +20,191 @@ function HomePage() {
     <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</button>
   </li>
 </ul>
+
 <div class="tab-content" id="pills-tabContent">
   <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">...</div>
   <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
   <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
-</div>
+
+  <form class="d-flex" style={{width:'40vw',marginLeft:'30vw'}}>
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
 </div>
 
-     );
+{/* For School */}
+
+<div class="card-group">
+  <div class="card">
+    <img src="./Aps.png" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Apply</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="./Aps.png" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Apply</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="./Aps.png" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Apply</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="./Aps.png" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Apply</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="./Aps.png" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Apply</small>
+    </div>
+  </div>
+</div>
+
+{/* For Colleges */}
+
+<div class="card-group">
+  <div class="card">
+    <img src="./Aps.png" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Apply</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="./Aps.png" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Apply</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="./Aps.png" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Apply</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="./Aps.png" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Apply</small>
+    </div>
+  </div>
+
+  <div class="card">
+    <img src="./Aps.png" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Apply</small>
+    </div>
+  </div>
+
+</div>
+
+
+{/* For Acadmy */}
+
+<div class="card-group">
+  <div class="card">
+    <img src="./Aps.png" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Apply</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="./Aps.png" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Apply</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="./Aps.png" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Apply</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="./Aps.png" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Apply</small>
+    </div>
+  </div>
+
+  <div class="card">
+    <img src="./Aps.png" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Apply</small>
+    </div>
+  </div>
+</div>
+
+
+</div>
+
+    );
 }
 
 export default HomePage;
