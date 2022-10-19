29b46ab050dc2daaf799721c66a23e526c1d94ee
Syed Kashif Ali Shah <114756954+kashif666@users.noreply.github.com>
2022-10-19 09:02:20 -0700

    some change

---
+++ src/Profile.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Profile() {
+    return ( 
+        <Link to={Profile}>
+        <h1>This is Profile page</h1>
+
+        </Link>
+     );
+}
+
+export default Profile;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.js";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
